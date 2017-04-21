fdecf5d2477a176ea6dac2303dde68844f4002eb
wheats2014 <532770034@qq.com>
2017-04-21 14:34:19 +0800

    add session mysql npm

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -15,12 +15,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended : false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name : 'session',
+  keys : [ "username" ],
+  secret : "session",
+  // Cookie Options
+  maxAge : 24 * 60 * 60 * 1000 // 24 hours
+}));
+
+app.use(function(req, res, next) {
+
+  console.log(req.session.username);
+
+  // session 判断
+
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,12 @@ var router = express.Router();
 var mysql = require('mysql');
 var connection = require('../database/connection');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-
-    connection.query('select * from user', function(error, results, fields) {
-      if (error) {
-        throw error;
-      }
-      res.render('users', {results : results});
-    });
-    connection.end();
+  connection.query('select * from user', function(err, rows, fields) {
+    if (err)
+      throw err;
+    res.render('users', {results : rows[0]});
   });
 
 });
