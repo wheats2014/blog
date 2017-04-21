12f918edb9e81921ec5c9fe50fba061e4399449e
wheats2014 <532770034@qq.com>
2017-04-21 13:48:16 +0800

    add mysql npm

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = require('../database/connection');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+
+    connection.query('select * from user', function(error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      res.render('users', {results : results});
+    });
+    connection.end();
+  });
+
 });
 
 module.exports = router;
